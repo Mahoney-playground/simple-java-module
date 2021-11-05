2fd7711465616a5bb7a8d1e9383426abcec60acb
Robert Elliot <rob@lidalia.org.uk>
2021-11-05 15:41:18 +0000

    Make Dockerfile execution more correct
    Use Entrypoint exec form so that:
    * Signals are captured
    * PID is the Java process pid
    
    Use a generated file so that args in the Dockerfile, and ENV at runtime, are
    respected.

--- src/com.greetings/com/greetings/Main.java
+++ src/com.greetings/com/greetings/Main.java
@@ -1,7 +1,15 @@
 package com.greetings;
 import org.astro.World;
+
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.format("Greetings %s!%n", World.name());
+        System.out.println("Args: "+Arrays.asList(args));
+        System.out.println("Props: "+System.getProperties());
+        if (args.length > 0 && args[0].equals("sleep")) {
+            Thread.sleep(1000 * 60);
+        }
     }
 }
